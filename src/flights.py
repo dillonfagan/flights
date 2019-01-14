5faebcfe2e4db0b0e114b90beb65f47d748ad012
Dillon Fagan <dillonfagan@gmail.com>
2019-01-13 19:19:53 -0500

    Create src folder, create flights.py file

---
+++ src/flights.py
@@ -0,0 +1,16 @@
+'''
+We will write at least the following functions to implement our Flights program:
+
+data(filename) -> returns a matrix of all the data contained in a .csv file, given the filename
+print_table(data) -> prints a table containing data from the given matrix
+print_table_row(data, cell_width) -> prints a row of table cells with a given width
+column_widths(data) -> returns a dictionary {column header: column width}
+
+All of the functions above should be implemented in their own separate file, since
+this code could actually be used for programs other than this Flights program. It makes
+a lot of sense to close it off so that it could be imported into other files if need be.
+Let's call this new file "tables.py".
+
+When this code is completed, you'll import it into this file and write your Flights program, 
+which you should be able to do on your own.
+'''
